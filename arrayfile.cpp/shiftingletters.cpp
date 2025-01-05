016363c54ece0c0df8a5b176dea45909bfa9a762
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-05 20:30:07 +0000

    adding shifting letters 1 and shifting letters 2 both

--- arrayfile.cpp/shiftingletters.cpp
+++ arrayfile.cpp/shiftingletters.cpp
@@ -1,37 +1,9 @@
 // class Solution {
 // public:
-//     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-//         int n = s.length();
-
-//         vector<int> diff(n, 0);
-
-//         for (auto& query : shifts) {
-//             int L = query[0];
-//             int R = query[1];
-//             int dir = query[2];
-//             int x;
-
-//             if (dir == 0)
-//                 x = -1;
-//             else
-//                 x = 1;
-
-//             diff[L] += x;
-//             if (R + 1 < n)
-//                 diff[R + 1] -= x;
-//         }
-
-//         // cumulative sum;
-//         for (int i = 1; i < n; i++)
-//             diff[i] += diff[i - 1];
-
-//         // applying shifting to the string
-//         for (int i = 0; i < n; i++) {
-//             int shift = diff[i] % 26; // wrap around;
-
-//             if (shift < 0)
-//                 shift += 26; // wrap around
-
+//     string shiftingLetters(string s, vector<int>& shifts) {
+//         long long shift = 0;
+//         for (int i = s.length() - 1; i >= 0; i--) {
+//             shift += shifts[i];
 //             s[i] = (((s[i] - 'a') + shift) % 26) + 'a';
 //         }
 //         return s;
