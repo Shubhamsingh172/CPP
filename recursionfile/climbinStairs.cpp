0cefe2236412a4062b6b949739d6c803c11923fb
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-09 19:01:01 +0000

    climbing stairs problem but having tle limt.

---
+++ recursionfile/climbinStairs.cpp
@@ -0,0 +1,15 @@
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n < 0)
+//             return 0;
+
+//         if(n == 0)
+//             return 1;
+
+//         int one_step = climbStairs(n-1);
+//         int two_step = climbStairs(n-2);
+
+//         return one_step + two_step;
+//     }
+// };
