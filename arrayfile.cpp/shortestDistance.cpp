325e74517d6b032f9682ed4898ba018898576906
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-18 18:40:55 +0000

    not an optimal solution only 3ms or 4ms

---
+++ arrayfile.cpp/shortestDistance.cpp
@@ -0,0 +1,24 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         // int max = 0;
+//         // sort(nums.begin(), nums.end());
+//         // for(int i = 0; i < nums.size() - 1; i++)
+//         // {
+//         //     if((nums[i] - 1) * (nums[i + 1] -1) > max)
+//         //         max = (nums[i] - 1) * (nums[i + 1] - 1);
+//         // }
+//         // return max;
+
+//         // int max = 0;
+//         // for(int i = 0; i < nums.size(); i++)
+//         // {
+//         //     for(int j = i + 1; j < nums.size(); j++)
+//         //     {
+//         //         if((nums[i] - 1) * (nums[j] -1) > max)
+//         //             max = (nums[i] - 1) * (nums[j] - 1);
+//         //     }
+//         // }
+//         // return max;
+//     }
+// };
