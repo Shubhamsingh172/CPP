162f272fba0489caa3f770b9b83293fbfd30c16a
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-20 17:58:35 +0000

    adding description in the code

--- arrayfile.cpp/nextPermutation.cpp
+++ arrayfile.cpp/nextPermutation.cpp
@@ -3,7 +3,8 @@
 //     void nextPermutation(vector<int>& nums) {
 //         int n = nums.size();
 
-//         int target_index = -1; // gola index
+//         int target_index = -1; // gola index which will find the number at ith position which greater than i - 1 and store 
+            // i - 1 in target_index variable.
 //         for(int i = n - 1; i > 0; i--)
 //         {
 //             if(nums[i] > nums[i-1])
@@ -13,9 +14,10 @@
 //             }
 //         }
 
-//         if(target_index != -1)
+//         if(target_index != -1) // if somehow number is not found and in the above for loop it reaches to last then directly reverse 
 //         {
-//             int right_index = target_index;
+//             int right_index = target_index; // 1 3 4 5 2 here first we look for number from right is greater than its i -1 or 
+                //not if yes then swap it with each other and reversing the rest of the numbers
 //             for(int j = n - 1; j >= target_index; j--)
 //             {
 //                 if(nums[j] > nums[target_index])
