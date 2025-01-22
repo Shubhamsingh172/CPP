56b44f5fcc617bd9da36e4810b24acc5b85e8ce2
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-22 14:15:10 +0000

    Reverse String in a Word optimal solution using two pointers

---
+++ strings/reverseStringWord2.cpp
@@ -0,0 +1,29 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         int i = 0;
+//         int l = 0, r = 0;
+//         int n = s.length();
+//         //first reverse a string 
+//         reverse(s.begin(),s.end());
+
+//         while(i < n)
+//         {
+//             while(i < n && s[i] != ' ')
+//             {
+//                 s[r++] = s[i++];
+//             }
+
+//             if(l < r)
+//             {
+//                 reverse(s.begin() + l, s.begin() + r);
+//                 s[r] = ' ';
+//                 r++;
+//                 l = r;
+//             }
+//             i++;
+//         }
+//         s = s.substr(0, r - 1);
+//         return s;
+//     }
+// };
