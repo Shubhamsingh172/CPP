792be3fb353df8c05f2cf24bfc9abf36487e01bb
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-26 18:16:34 +0000

    added approach 2

--- strings/checkTwoString1662.cpp
+++ strings/checkTwoString1662.cpp
@@ -11,5 +11,40 @@
 //             s2 += word2[i];
 
 //         return s1 == s2;
+
+
+//      approach 2
+//         int m = word1.size();
+//         int n = word2.size();
+
+//         int w1 = 0;
+//         int w2 = 0;
+//         int i = 0; 
+//         int j = 0;
+
+//         while(w1 < m && w2 < n)
+//         {
+//             if(word1[w1][i] != word2[w2][j])
+//                 return false;
+//             i++;
+//             j++;
+
+//             if(i == word1[w1].length())
+//             {
+//                 i = 0;
+//                 w1++;
+//             }
+
+//             if(j == word2[w2].length())
+//             {
+//                 j = 0;
+//                 w2++;
+//             }
+//         }
+//         if(w1 == word1.size() && w2 == word2.size())
+//             return true;
+//         return false;
 //     }
 // };
+
+
