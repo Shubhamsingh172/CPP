8db5eccafb152c43b828cbff80b7c12b5c5c4171
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-29 19:17:15 +0000

    Permutations second solution

---
+++ Backtracking/permutationsSecSol.cpp
@@ -0,0 +1,26 @@
+// class Solution {
+// public:
+//     vector<vector<int>>result;
+//     int n;
+
+//     void solve(int idx, vector<int>& nums)
+//     {
+//         if(idx == n)
+//         {
+//             result.push_back(nums);
+//             return;
+//         }
+
+//         for(int i = idx; i < n; i++)
+//         {
+//             swap(nums[idx],nums[i]);
+//             solve(idx + 1, nums);
+//             swap(nums[idx],nums[i]);
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         n = nums.size();
+//         solve(0, nums);
+//         return result;
+//     }
+// };
