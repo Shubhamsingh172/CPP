e6eac936ebbd54a4a20b3bfc38fe5005b5f80a9f
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-30 17:56:59 +0000

    the number of beautiful subset program solution

---
+++ Backtracking/beautifulSubset.cpp
@@ -0,0 +1,32 @@
+// class Solution {
+// public:
+//     int result;
+//     int K;
+//     void solve(int idx, vector<int>& nums, unordered_map<int, int>& mp)
+//     {
+//         if(idx >= nums.size())
+//         {
+//             result++;
+//             return;
+//         }
+
+//         //not take
+//         solve(idx + 1, nums, mp);
+
+//         if(!mp[nums[idx] - K] && !mp[nums[idx] + K])
+//         {
+//             mp[nums[idx]]++; //do
+//             solve(idx + 1, nums, mp); //explore
+//             mp[nums[idx]]--; // undo
+//         }
+//     }
+//     int beautifulSubsets(vector<int>& nums, int k) {
+//         result = 0;
+//         K = k;
+
+//         unordered_map<int, int> mp;
+//         solve(0, nums, mp);
+
+//         return result - 1; // excluding empty set
+//     }
+// };
