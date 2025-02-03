59737dba08322c275d7e57a6d2a46e6ef5450e7a
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-03 10:50:38 +0000

    palindrome 2 program in cpp

---
+++ arrayfile.cpp/palindrome3feb.cpp
@@ -0,0 +1,30 @@
+// class Solution {
+// public:
+//     vector<vector<int>>result;
+//     int n;
+
+//     void solve(int idx, vector<int>&nums)
+//     {
+//         if(idx == n)
+//         {
+//             result.push_back(nums);
+//             return;
+//         }
+
+//         unordered_set<int>st;
+//         for(int i = idx; i < n; i++)
+//         {
+//             if(st.count(nums[i]) > 0)
+//                 continue;
+//             st.insert(nums[i]);
+//             swap(nums[i], nums[idx]);
+//             solve(idx + 1, nums);
+//             swap(nums[i], nums[idx]);
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         n = nums.size();
+//         solve(0, nums);
+//         return result;
+//     }
+// }; only for just submission to maintain the streak so take it as it is.
